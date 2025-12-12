2c5687c850d74f1520b313d3e96e2b5ce3821c39
Nicole C. <nmcanson@gmail.com>
2025-12-11 20:30:34 -0800

    Third commit

--- Final2.cpp
+++ Final2.cpp
@@ -14,9 +14,21 @@ string names[] = {"Alex","Jamie","Taylor","Morgan","Riley"};
 string drinks[] = {"Latte","Mocha","Espresso"};
 
 int main() {
-    srand(time(0);
+    srand(time(0));
 
-    Node* head = nullprt;
+    Node* head = nullptr;
     Node* tail = nullptr;
 
+    for (int i = 0; i < 3, i++) {
+        Node* n = new Node;
+        n->name = names[rand() % 5];
+        n->drink = drinks[rand() % 3];
+        n->next = head;
+        head = n;
+    }
+
+    if (head != nullptr) {
+        cout << "Serving " << head-name << " (" << head->drink << ")\n";
+        head = head->next;
+    }
 }
