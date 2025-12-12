aa10a68b2a7ceb05edb2b2ae56cd14c0269e9629
Nicole C. <nmcanson@gmail.com>
2025-12-11 20:50:12 -0800

    Sixth commit

--- Final2.cpp
+++ Final2.cpp
@@ -13,39 +13,45 @@ struct Node {
 string names[] = {"Alex","Jamie","Taylor","Morgan","Riley"};
 string drinks[] = {"Latte","Mocha","Espresso"};
 
+void enqueue(Node* &head, Node* &tail) {
+    Node* n = new Node;
+    n->name = names[rand() % 5];
+    n->drink = drinks[rand() % 3];
+    n->next = nullptr;
+        
+    if (tail == nullptr) {
+        head = tail = n;
+    } else {
+        tail->next = n;
+        tail = n;
+    }
+    cout << n->name << " joined the coffee queue\n";
+}
+
+void serve(Node* &head, Node* &tail) {
+
+    
+}
+
 int main() {
     srand(time(0));
 
     Node* head = nullptr;
     Node* tail = nullptr;
 
-    for (int i = 0; i < 3; i++) {
-        Node* n = new Node;
-        n->name = names[rand() % 5];
-        n->drink = drinks[rand() % 3];
-        n->next = nullptr;
-        
-        if (tail == nullptr) {
-            head = tail = n;
-        } else {
-            tail->next = n;
-            tail = n;
-        }
-    }
-
     for (int round = 1; round <= 10; round++) {
         cout << "\nRound " << round << endl;
 
         if (head != nullptr) {
             cout << "Serving " << head->name
-                 << " ()" << head->drink << ")\n";
-            Node*temp = head;
+                 << " (" << head->drink << ")\n";
+            Node* temp = head;
             head = head->next;
             if (head == nullptr) tail = nullptr;
             delete temp;
         } else {
             cout << "No customer to serve\n";
         }
-        
+
     }
 }
