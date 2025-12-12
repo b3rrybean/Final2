633ff15e7fb3c27f7d4a0025f6132c6afc6583e0
Nicole C. <nmcanson@gmail.com>
2025-12-11 20:26:54 -0800

    Second commit

---
+++ Final2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+struct Node {
+    string name;
+    string drink;
+    Node* next;
+};
+
+string names[] = {"Alex","Jamie","Taylor","Morgan","Riley"};
+string drinks[] = {"Latte","Mocha","Espresso"};
+
+int main() {
+    srand(time(0);
+
+    Node* head = nullprt;
+    Node* tail = nullptr;
+
+}
